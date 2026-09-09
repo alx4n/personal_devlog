d4e28b9883a5b858466192350b9ca987155805ad
Alex Bratek <nbratek99@gmail.com>
2026-09-09 19:35:44 -0400

    Edit metadata and remove intro message

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,9 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Alex Bratek CS414 Personal Devlog",
+	url: "https://alx4n.github.io/personal_devlog/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am tracking my progress and contributions to my capstone game.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Alex Bratek"
 	}
 }
